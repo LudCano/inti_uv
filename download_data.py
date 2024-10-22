130f0ba046bb9901acd2e3e1e9a8d6ddbcc0744c
Ludving Cano Fernandez <66972062+LudCano@users.noreply.github.com>
2024-10-22 10:18:56 -0400

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,9 +1,13 @@
 import requests
+import os
 
 csv_url = 'https://docs.google.com/spreadsheets/d/16bEpYUm1Z2FpAjQOAQi8JEC3tRvOoAJZgV005v6RuEw/gviz/tq?tqx=out:csv'
 
 # Realiza la solicitud GET para descargar el CSV
 response = requests.get(csv_url)
+
+if os.path.exists('intiuv_data.csv'):
+    os.remove('intiuv_data.csv')
 
 # Verifica si la solicitud fue exitosa
 if response.status_code == 200:
